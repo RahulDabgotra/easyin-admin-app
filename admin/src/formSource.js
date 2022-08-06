62d50e72019a5e6e82e78cc080fe95bd033d1e78
Rahul Dabgotra <rahuldabgo@gmail.com>
2022-08-06 10:59:16 +0530

    react app

---
+++ admin/src/formSource.js
@@ -0,0 +1,149 @@
+export const userInputs = [
+  {
+    id: "username",
+    label: "Username",
+    type: "text",
+    placeholder: "Name",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "mail@gmail.com",
+  },
+  {
+    id: "phone",
+    label: "Phone",
+    type: "text",
+    placeholder: "+91 00000 00000",
+  },
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+    placeholder: "password",
+  },
+  {
+    id: "country",
+    label: "Country",
+    type: "text",
+    placeholder: "India",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "Jammu",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
+
+export const hotelInputs = [
+  {
+    id: "name",
+    label: "Name",
+    type: "text",
+    placeholder: "",
+  },
+  {
+    id: "type",
+    label: "Type",
+    type: "text",
+    placeholder: "",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "",
+  },
+  {
+    id: "address",
+    label: "Address",
+    type: "text",
+    placeholder: "",
+  },
+  {
+    id: "distance",
+    label: "Distance from City Center",
+    type: "text",
+    placeholder: "500",
+  },
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "text",
+    placeholder: "description",
+  },
+  {
+    id: "cheapestPrice",
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+];
+
+export const roomInputs = [
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "Table",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "text",
+    placeholder: "About Tables",
+  },
+  {
+    id: "price",
+    label: "Price",
+    type: "number",
+    placeholder: "100",
+  },
+  {
+    id: "maxPeople",
+    label: "Max People",
+    type: "number",
+    placeholder: "2",
+  },
+];
